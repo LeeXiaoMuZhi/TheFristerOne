d1b0288d0a27e5032f4d475d4ad9cfdfb09064c1
qianfeng <qianfeng@youdao.local>
2016-03-24 15:05:35 +0800

    sdahfajkosdfhasdjk lsd;g

---
+++ OneTest/OneTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneTest
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright © 2016年 KRT.CT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneTest/OneTest/LSViewController.m
@@ -0,0 +1,21 @@
+//
+//  LSViewController.m
+//  OneTest
+//
+//  Created by qianfeng on 16/3/24.
+//  Copyright © 2016年 KRT.CT. All rights reserved.
+//
+
+#import "LSViewController.h"
+
+@implementation LSViewController
+
+//@dynamic property
+-(NSString *)hale
+{
+    NSLog(@"这是一个测试文件");
+    NSString *str=[[NSString alloc]init];
+    return str;
+
+}
+@end
